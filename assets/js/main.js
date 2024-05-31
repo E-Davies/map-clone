44ed20e701c3d6bb5b1c54114c1095d4b97355f4
e-davies <emmi.lou002@gmail.com>
2024-05-31 19:32:43 +0100

    added speed change function

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,23 +3,29 @@ const speed = document.getElementById('speed');
 const playBtn = document.getElementById('play');
 const pauseBtn = document.getElementById('pause');
 const stopBtn = document.getElementById('stop');
+let currentCharacter;
 
+const sayInput = new SpeechSynthesisUtterance();
+sayInput.addEventListener('boundary', e => {
+    currentCharacter = e.charIndex
+});
 
 playBtn.addEventListener('click', () => {
     playText(input.value);
 });
-
 pauseBtn.addEventListener('click', pauseText);
-
 stopBtn.addEventListener('click', stopText);
+speed.addEventListener('input', () => {
+    stopText()
+    playText(sayInput.text.substring(currentCharacter))
+});
 
 function playText(input) {
        if(speechSynthesis.paused) { 
         return speechSynthesis.resume();
     };
-    const sayInput = new SpeechSynthesisUtterance(input);
+    sayInput.text = input;
     sayInput.rate = speed.value || 1;
-
     speechSynthesis.speak(sayInput);
 };
 
